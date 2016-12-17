a7a75fd6dc470ad948febf01b7bec036d07744c1
muneel <muneel@hotmail.com>
2016-12-17 14:54:36 -0500

    Added setup.py support

---
+++ setup.py
@@ -0,0 +1,16 @@
+#coding:utf-8
+
+from setuptools import setup, find_packages
+
+setup(	name="interface-utilization",
+		version="0.1",
+		packages=find_packages(),
+		install_requires=[],
+		py_modules = ['intutil'],
+		entry_points = {
+       			 'console_scripts': ['intutil=intutil:main'],
+   			 },
+		author="Muneel",
+		author_email="muneel@hotmail.com",
+		description="Tool to get Network Interface Utilization"
+		)
